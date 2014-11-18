76b37c0834f0bef773af975a7b3da825046fbbe6
Andrei <andreialexandrovich@gmail.com>
2014-11-19 00:59:39 +0300

    - Basic css added - Mono torrent references and libraries added

--- WebTorrent.Domain/Class1.cs
+++ WebTorrent.Domain/Class1.cs
@@ -1,6 +1,15 @@
-﻿namespace WebTorrent.Domain
+﻿using MonoTorrent.BEncoding;
+using MonoTorrent.Client.Encryption;
+using MonoTorrent.Client.Tracker;
+using MonoTorrent.Common;
+
+namespace WebTorrent.Domain
 {
     public class Class1
     {
+        public void Test()
+        {
+            Torrent torrent = null;
+        }
     }
 }
