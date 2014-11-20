ecefdcddb67bf62242b7794578dbe714e30621db
Andrei <andreialexandrovich@gmail.com>
2014-11-21 01:18:12 +0300

    Html template added and css bundles configured, created default html page

--- WebTorrent.WebApp/App_Start/BundleConfig.cs
+++ WebTorrent.WebApp/App_Start/BundleConfig.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Optimization;
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Optimization;
 
 namespace WebTorrent.WebApp.App_Start
 {
@@ -7,25 +9,62 @@ namespace WebTorrent.WebApp.App_Start
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
-
-            // Set EnableOptimizations to false for debugging. For more information,
-            // visit http://go.microsoft.com/fwlink/?LinkId=301862
+#if DEBUG
+            BundleTable.EnableOptimizations = false;
+#else
             BundleTable.EnableOptimizations = true;
+#endif
+
+            var globalMandatoryStyles = new[]
+                          {
+                              "~/Styles/Metronic/global/plugins/font-awesome/css/font-awesome.min.css",
+                              "~/Styles/Metronic/global/plugins/simple-line-icons/simple-line-icons.min.css",
+                              "~/Styles/Metronic/global/plugins/bootstrap/css/bootstrap.min.css",
+                              "~/Styles/Metronic/global/plugins/uniform/css/uniform.default.css",
+                              "~/Styles/Metronic/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css",
+                              "~/Styles/Metronic/global/css/components.css",
+                              "~/Styles/Metronic/admin/layout/css/layout.css",
+                              "~/Styles/Metronic/admin/layout/css/themes/default.css",
+                              "~/Styles/Metronic/admin/layout/css/custom.css",
+
+                             
+                          };
+
+            bundles.Add(new StyleBundle("~/css") { Orderer = new AsIsBundleOrderer() }
+               .IncludeRewriteCssStyles(globalMandatoryStyles)
+               );
+
+        }
+    }
+
+
+    public class AsIsBundleOrderer : IBundleOrderer
+    {
+        public IEnumerable<BundleFile> OrderFiles(BundleContext context, IEnumerable<BundleFile> files)
+        {
+            return files;
+        }
+    }
+
+    public static class BundlesExtensions
+    {
+        public static Bundle IncludeRewriteCss(this Bundle bundle, string path)
+        {
+            return bundle.Include(path, new CssRewriteUrlTransform());
+        }
+
+        public static Bundle IncludeRewriteCssStyles(this Bundle bundle, params string[] paths)
+        {
+            foreach (var path in paths)
+                bundle.Include(path, new CssRewriteUrlTransform());
+
+            return bundle;
+        }
+
+        public static Bundle IncludeAction(this Bundle bundle, Func<Bundle, Bundle> func, params IItemTransform[] transforms)
+        {
+            return func(bundle);
         }
     }
 }
---
+++ WebTorrent.WebApp/Controllers/Common/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace WebTorrent.WebApp.Controllers.Common
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
